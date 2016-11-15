fbd59e6e9fd1692100a478cd943d92671764affb
icetec <pit@ice-technologies.de>
2016-11-15 10:29:26 +0100

    General Update

---
+++ Tutorials/TutorialNavMesh/SimpleDemoScene/Scripts/ICECreatureControlSimpleDemo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using ICE;
+using ICE.Creatures;
+using ICE.Creatures.Objects;
+using ICE.Creatures.EnumTypes;
+
+namespace ICE.Creatures.Demo
+{
+	public class ICECreatureControlDemo : MonoBehaviour {
+
+		void Awake()
+		{
+			TongleMovePointer( false );
+			TongleTrails( false );
+		}
+
+		void Update()
+		{
+
+		}
+
+		public void TongleMovePointer( bool value )
+		{
+
+		}
+
+		public void TongleTrails( bool value )
+		{
+
+		}
+	}
+}
